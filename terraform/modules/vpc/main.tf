ff4f4bc51a1d047a872984d7b387366a78dbbf2b
gjfuente <gjfuente@amazon.com>
2026-02-27 10:21:09 -0500

    Add S3 Vectors VPC endpoint configuration to Terraform module

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -227,3 +227,16 @@ resource "aws_vpc_endpoint" "bedrock_runtime" {
     Name = "${var.project_name}-${var.environment}-bedrock-runtime-vpce"
   }
 }
+
+resource "aws_vpc_endpoint" "s3vectors" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.s3vectors"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-s3vectors-vpce"
+  }
+}
